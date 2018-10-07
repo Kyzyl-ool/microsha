9e137fb9ca9770d4d19f6684f915bbdeb85e91ab
Kyzyl-ool <kyzyloolk@mail.ru>
2018-10-07 11:26:19 +0300

    Initial Commit

---
+++ microsha/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  microsha
+//
+//  Created by Кежик Кызыл-оол on 07/10/2018.
+//  Copyright © 2018 Кежик Кызыл-оол. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
